72b903d7f4f16e3e2b3f1a95fe076d888f5a4eed
2012ceric <2012ceric@gmail.com>
2019-11-06 15:00:00 -0500

    removed old folders

--- ex1-1/1-1.c
+++
@@ -1,6 +0,0 @@
-// exercise on page 8
-#include <stdio.h>
-
-int main(){
-	printf("Hello World!\n");
-}
--- ex1-7/1-7.c
+++
@@ -1,8 +0,0 @@
-// exercise on page 17
-#include <stdio.h>
-
-int main(){
-	int c = EOF;
-
-	printf("EOF: %d\n", c);
-}
